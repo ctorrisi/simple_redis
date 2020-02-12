1794e44478bd6b79dfe4620ad3c79bd5812fe9ca
Corey Torrisi <3690732+ctorrisi@users.noreply.github.com>
2020-02-12 15:14:17 +1100

    Updated.

--- src/sentinel.rs
+++ src/sentinel.rs
@@ -4,56 +4,83 @@
 //!
 
 use crate::client;
-use crate::types::RedisError;
 use redis;
 use client::Client;
 
 /// Encapsulates the metadata and connections related to the Sentinels
 pub struct Sentinel {
+    /// Encapsulates the metadata and connections related to the Sentinels
     pub sentinel_addrs: Vec<String>,
+    /// Encapsulates the metadata and connections related to the Sentinels
     pub master: String,
-    pub master_client: Option<Client>,
+    /// Encapsulates the metadata and connections related to the Sentinels
+    pub master_client: Client,
 }
 
 impl Sentinel {
 
-    /// Retrieves the master client as reported by the Sentinel(s).
-    pub fn get_client(&mut self) -> Result<&Option<Client>, RedisError>
+    /// Encapsulates the metadata and connections related to the Sentinels
+    pub fn new(sentinel_addrs: Vec<String>, master: String) -> Sentinel
     {
-        if self.master_client.is_none() || !self.master_client.unwrap().is_connection_open() {
-            for sentinel_addr in &self.sentinel_addrs {
-                let sentinel_client = redis::Client::open(sentinel_addr.as_str()).unwrap();
-                let mut con = sentinel_client.get_connection().unwrap();
-
-                let current_master_info = redis::cmd("SENTINEL")
-                    .arg("get-master-addr-by-name")
-                    .arg(self.master.as_str()).query(&mut con);
-
-                let client: Option<Client> = match current_master_info {
-                    Ok(addr) => {
-                        let addr: Vec<String> = addr;
-                        let current_master_socket = format!("{}:{}", &addr[0], &addr[1]);
-                        println!("Current master socket: {}", current_master_socket.as_str());
-                        let mut client = client::create(current_master_socket.as_str())?;
-                        if client.is_connection_open() {
-                            Some(client)
-                        } else {
-                            None
-                        }
-                    },
-                    Err(e) => {
-                        println!("Failed to get current master from sentinel: {:?}", e);
-                        None
-                    }
-                };
-
-                if client.is_some() {
-                    self.master_client = Some(client.unwrap());
-                    break;
-                }
-            }
+        let master_client = Sentinel::new_client(sentinel_addrs.clone(), master.clone()).unwrap();
+        Sentinel {
+            sentinel_addrs,
+            master,
+            master_client
         }
+    }
 
-        Ok(&self.master_client)
+    /// Encapsulates the metadata and connections related to the Sentinels
+    fn new_client(sentinel_addrs: Vec<String>, master: String) -> Option<Client> {
+        for sentinel_addr in &sentinel_addrs {
+            let sentinel_client = redis::Client::open(sentinel_addr.as_str()).unwrap();
+            let mut con = sentinel_client.get_connection().unwrap();
+
+            let current_master_info = redis::cmd("SENTINEL")
+                .arg("get-master-addr-by-name")
+                .arg(master.as_str()).query(&mut con);
+
+            match current_master_info {
+                Ok(addr) => {
+                    let addr: Vec<String> = addr;
+                    let current_master_socket = format!("{}:{}", &addr[0], &addr[1]);
+                    println!("Current master socket: {}", current_master_socket.as_str());
+                    let mut client = client::create(current_master_socket.as_str());
+                    match client  {
+                        Ok(mut c) => {
+                            if c.is_connection_open() {
+                                return Some(c)
+                            }
+                        },
+                        Err(e) => {
+                            println!("Error: {:?}", e);
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to get current master from sentinel: {:?}", e);
+                }
+            };
+        }
+        None
+    }
+
+    /// Retrieves the master client as reported by the Sentinel(s).
+    pub fn get_client(&mut self) -> Option<&Client>
+    {
+        let not_connected = !self.master_client.is_connection_open();
+        if not_connected {
+            match Sentinel::new_client(self.sentinel_addrs.clone(), self.master.clone()) {
+                Some(client) => {
+                    self.master_client = client;
+                    return Some(&self.master_client)
+                },
+                None => {
+                    println!("No client!");
+                    return None
+                }
+            };
+        }
+        Some(&self.master_client)
     }
 }

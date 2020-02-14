5fc5a32092687b44a51526262aeab970d7fd574e
Corey Torrisi <3690732+ctorrisi@users.noreply.github.com>
2020-02-14 17:05:42 +1100

    Update

--- src/sentinel.rs
+++ src/sentinel.rs
@@ -102,8 +102,7 @@ impl Sentinel {
     fn create_new_client(master_addr: &String) -> Option<Client>
     {
         if !master_addr.is_empty() {
-            let mut client = client::create("redis://127.0.0.1:6379/");
-//            let mut client = client::create(master_addr.as_str());
+            let mut client = client::create(master_addr.as_str());
             match client  {
                 Ok(mut c) => {
                     if Sentinel::is_connection_open(&mut c) {
